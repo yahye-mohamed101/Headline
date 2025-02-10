2e5ca11d0606c88a69d5817f383ae1c63bae4fa0
Buster <yahyemohamed2002@gmail.com>
2025-02-10 14:50:06 -0600

    modified vite confi

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
     outDir: 'dist',
     assetsDir: 'assets',
     sourcemap: false,
-    minify: 'terser',
+    minify: 'esbuild', // Changed from 'terser' to 'esbuild'
     target: 'es2018',
     cssCodeSplit: true,
     rollupOptions: {
